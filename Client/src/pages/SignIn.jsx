b921905496e326777df3e17b1546338e3a7cf5b7
Mitchell-610 <Mheit602@gmail.com>
2024-10-15 21:04:35 -0400

    CSS applied for profile page.

--- Client/src/pages/SignIn.jsx
+++ Client/src/pages/SignIn.jsx
@@ -2,20 +2,23 @@ import './signin.css';
 
 export default function SignIn() {
   return (
-    <div>
+    <div className='lin'>
       <div>
         <h1>Profile</h1>
       </div>
       <div className='login'>
         <form>
+
           <div className="form-group">
             <label htmlFor="username">Username: (Username)</label>
             <input type="text" id="username" name="user_name" />
           </div>
+
           <div className="form-group">
             <label htmlFor="password">Password: (********)</label>
             <input type="password" id="password" name="password" />
           </div>
+          
           <button type="submit">Submit</button>
         </form>
       </div>
