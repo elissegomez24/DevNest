ec62d6f9b2048113137ee5d1f510e037728f6f50
Aden Neal <adenocbenji@gmail.com>
2024-10-27 17:38:20 -0500

    job board start

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,46 +1,48 @@
 const { GraphQLError } = require('graphql');
 const jwt = require('jsonwebtoken');
 const { User } = require('../models');
-const { GraphQLError } = require('graphql');
 
-// Set token secret and expiration date
-const secret = 'mysecretsshhhhh';
+// Using environment variable for security (recommended)
+const secret = process.env.JWT_SECRET || 'mysecretsshhhhh';
 const expiration = '2h';
 
-const  AuthenticationError = new GraphQLError('Could not authenticate user.', {
+const AuthenticationError = new GraphQLError('Could not authenticate user.', {
     extensions: {
-    code: 'UNAUTHENTICATED',
-    },
-})
+        code: 'UNAUTHENTICATED',
+    }
+});
 
 const authMiddleware = async ({ req }) => {
-    // Set token to be used for authentication
     let token = req.headers.authorization || '';
 
-        // We split the token string into an array and return actual token
-        if (req.headers.authorization) {
-            token = token.split(' ').pop().trim();
-        }
+    if (req.headers.authorization) {
+        token = token.split(' ').pop().trim();
+    }
 
-        if (!token) {
-            return req;
-        }
-
-        // if token can be verified, add the decoded user's data to the request so it can be accessed in the resolver
-        try {
-            const { data } = jwt.verify(token, secret, { maxAge: expiration });
-            req.user = data;
-        } catch {
-            console.log('Invalid token');
-        }
-
-        // return the request object so it can be passed to the resolver as `context`
+    if (!token) {
         return req;
-    },
-    signToken: function ({ email, name, _id }) {
-        const payload = { email, name, _id };
-        return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-    },
+    }
+
+    try {
+        const { data } = jwt.verify(token, secret, { maxAge: expiration });
+        const user = await User.findById(data._id);
+        
+        if (!user) {
+            throw new AuthenticationError;
+        }
+        
+        req.user = data;
+    } catch (error) {
+        console.log('Authentication error:', error.message);
+        throw new AuthenticationError;
+    }
+
+    return req;
+};
+
+const signToken = function ({ email, name, _id }) {
+    const payload = { email, name, _id };
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
 };
 
 module.exports = { authMiddleware, signToken, AuthenticationError };
